d977bb1c625ba81c3ac2722518992cbba0be4ee9
Maël ARNAUD <mael.arnaud@gmail.com>
2018-10-26 23:12:42 +0200

    Adds first animations and new props

--- Character.gd
+++ Character.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
 var speed = 200
+var max_speed = 1000
 var velocity = Vector2(0, 10)
 
 func _ready():
 	set_fixed_process(true)
+	get_node("AnimationPlayer").play("Run1")
 
 func _fixed_process(delta):
-	velocity.x += delta * speed
+	if velocity.x < max_speed:
+		velocity.x += delta * speed
 	velocity.y += delta * speed
 	move_and_slide(velocity)
