fff35338a18ed7ba9f92643c6f377633201e3ef6
Maël ARNAUD <mael.arnaud@gmail.com>
2018-10-27 17:10:18 +0200

    Objects are now destructibles

--- Item.gd
+++ Item.gd
@@ -3,7 +3,13 @@ extends Node
 export var speed_affection = 0.0
 export var rads_affection = 0.0
 export var is_bumper = false
+export var is_destructible = true
 
 func _on_Area_body_enter( body ):
 	body.affect_speed(speed_affection, is_bumper)
 	body.affect_rads(rads_affection)
+	if is_destructible:
+		destroy()
+
+func destroy():
+	get_node("AnimationPlayer").play("Destroy")
