806d090c50c8c8a1171b03463a95ebda8b73670a
Maël ARNAUD <mael.arnaud@gmail.com>
2018-10-27 10:24:13 +0200

    Adds objects

--- Item.gd
+++ Item.gd
@@ -1,7 +1,7 @@
 extends Node
 
-var speed_affection = 0.0
-var rads_affection = 0.0
+export var speed_affection = 0.0
+export var rads_affection = 0.0
 
 func _on_Area_body_enter( body ):
 	body.affect_speed(speed_affection)
