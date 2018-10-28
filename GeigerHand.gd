8da6b70361048832a0232027ac8ad902cf590964
Maël ARNAUD <mael.arnaud@gmail.com>
2018-10-28 09:49:55 +0100

    Fresh pics !

--- GeigerHand.gd
+++ GeigerHand.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-var rad_goal
+var rad_goal = 0
 
 func set_value(value):
 	# value is in %
@@ -14,3 +14,9 @@ func _process(delta):
 		set_rot(get_rot() + delta/2.5)
 	elif rad_goal < get_rot():
 		set_rot(get_rot() - delta/2.5)
+	if abs(get_rot() - rad_goal) < 0.15:
+		randomize()
+		var al_val = rand_range(5, 8)
+		if rand_range(-1, 1) < 0:
+			al_val *= -1
+		rad_goal = -(((rad_goal + al_val) / 100 * PI) - PI/2)
